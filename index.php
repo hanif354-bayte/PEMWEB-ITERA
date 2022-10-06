7db921bb80cd67d5ffb52cd42e14aa595586ef46
hanif354 <75107950+hanif354-bayte@users.noreply.github.com>
2022-10-07 02:13:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DATA</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  
+  <form action="action.php" method="GET">
+    <table align="center"> 
+      <tr align="center">
+        <td><h2> 
+            <b>PEMAIN SEPAK BOLA</b>
+        </h2></td>
+    </tr>
+      <tr> 
+        <td>
+        <table border="0" cellpadding="0" cellspacing="10" align="center">
+          <tr>
+            <td>NOMOR PUNGGUNG</td>
+            <td> : </td>
+            <td><input name="id" type="text" size="40" /></td>
+          </tr>
+          <tr>
+            <td>NAMA PEMAIN</td>
+            <td> : </td>
+            <td><input name="name" type="text" size="40" /></td>
+          </tr>
+          <tr>
+            <td colspan="4" align="center">
+            <button class="submit">SUBMIT</button>
+          </td>
+          </tr>
+        </table>
+        </td>
+      </tr>
+    </table>
+  </form>
+
+</body>
+</html>
