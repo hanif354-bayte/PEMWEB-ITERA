51350caa72ef73d9b6863b7190babcce8e37ebc1
hanif354 <75107950+hanif354-bayte@users.noreply.github.com>
2022-10-07 03:00:13 +0700

    Add files via upload

---
+++ action.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DATA</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  
+  <form>
+    <table align="center"> 
+      <tr align="center">
+        <td><h2> 
+            <b>HASIL PENCARIAN</b>
+        </h2></td>
+    </tr>
+      <tr> 
+        <td>
+        <table border="0" cellpadding="0" cellspacing="10" align="center">
+          <tr>
+            <td>FOTO PEMAIN</td>
+          </tr>
+          <tr>
+            <td><center><img src="https://akcdn.detik.net.id/community/media/visual/2022/03/17/cristiano-ronaldo_43.jpeg?w=700&q=90" width="300" height="200" /></center><center></center></td>
+          </tr>
+        </table>
+        <center><b>CRISTIANO RONALDO</b></center>
+      </td>
+      </tr>
+    </table>
+  </form>
+
+</body>
+</html>
